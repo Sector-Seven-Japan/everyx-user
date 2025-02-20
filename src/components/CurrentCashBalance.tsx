8b5fbe3c9623318834c740e1f74e4edf8fef1902
Deepak Yadav <123127859+dny5112001@users.noreply.github.com>
2025-02-21 01:06:33 +0530

    Update CurrentCashBalance.tsx

--- src/components/CurrentCashBalance.tsx
+++ src/components/CurrentCashBalance.tsx
@@ -1,24 +1,13 @@
 import Image from "next/image";
-import React from "react";
+import React, { useContext } from "react";
 import settingIcon from "../../public/Icons/settingIcon.png";
 import { useRouter, usePathname } from "next/navigation";
+import { AppContext } from "@/app/Context/AppContext";
 
-interface CurrentCashBalanceCardProps {
-  balance: number;
-  bestCaseBalance?: number;
-  cumulativeWinnings?: number;
-  avatar?: string;
-}
-
-const CurrentCashBalanceCard: React.FC<CurrentCashBalanceCardProps> = ({
-  balance,
-  bestCaseBalance,
-  cumulativeWinnings,
-  avatar,
-}) => {
+const CurrentCashBalanceCard: React.FC = () => {
   const router = useRouter();
+  const { userStats, userProfile } = useContext(AppContext);
   const pathname = usePathname();
-
   const handleSettingsClick = () => {
     router.push("/profile");
   };
@@ -26,10 +15,10 @@ const CurrentCashBalanceCard: React.FC<CurrentCashBalanceCardProps> = ({
   return (
     <>
       <div className="flex justify-center items-center mt-5 relative">
-        {avatar && (
+        {userProfile && (
           <div className="w-20 h-20 rounded-full overflow-hidden">
             <Image
-              src={avatar}
+              src={userProfile.avatar}
               alt="User Profile Pic"
               className="object-cover rounded-full"
               width={80}
@@ -40,65 +29,88 @@ const CurrentCashBalanceCard: React.FC<CurrentCashBalanceCardProps> = ({
         <Image
           src={settingIcon}
           alt="Setting icon"
-          className="absolute top-1 left-[65%] cursor-pointer"
+          className="absolute top-1 left-[65%]"
           onClick={handleSettingsClick}
         />
       </div>
       <p className="text-[15px] text-center mt-5">Current Cash Balance</p>
-
-      {/* Display Current Cash Balance */}
       <div className="flex justify-center mt-5 items-baseline font-bold">
         <span className="text-[34px]">
-          ${Math.floor(balance).toLocaleString()}
+          ${userStats?.fund_available.toString().split(".")[0]}
         </span>
         <span className="text-[30px]">
-          .{(balance % 1).toFixed(2).split(".")[1]}
+          .{userStats?.fund_available.toFixed(2).split(".")[1]}
         </span>
       </div>
-
-      {/* Conditional Section */}
-      {(pathname === "/deposit-withdrawal/portfolio" ||
-        pathname === "/deposit-withdrawal/history") && (
+      {pathname === "/deposit-withdrawal/portfolio" ||
+      pathname === "/deposit-withdrawal/history" ? (
         <div className="mt-5">
           {/* Top Dashed Border */}
-          <div className="border-t-2 border-dashed border-gray-400 w-full my-3"></div>
+          <div
+            className="border-t-2 border-dashed border-gray-400 w-full my-3"
+            style={{
+              maskImage:
+                "linear-gradient(to right, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.4))",
+              WebkitMaskImage:
+                "linear-gradient(to right, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.4))",
+            }}
+          ></div>
 
           <div className="my-5 space-y-4">
             {/* Best Case Cash Balance */}
-            {bestCaseBalance !== undefined && (
-              <div className="flex justify-between items-end">
-                <span>Best case cash balance</span>
-                <span className="font-bold">
-                  <span className="text-[22px]">
-                    ${Math.floor(bestCaseBalance).toLocaleString()}
-                  </span>
-                  <span className="text-[14px]">
-                    .{(bestCaseBalance % 1).toFixed(2).split(".")[1]}
-                  </span>
+            <div className="flex justify-between items-end">
+              <span>Best case cash balance</span>
+              <span className="font-bold">
+                <span className="text-[22px]">
+                  $
+                  {
+                    userStats?.best_case_fund_available
+                      .toLocaleString()
+                      .split(".")[0]
+                  }
                 </span>
-              </div>
-            )}
+                <span className="text-[14px]">
+                  .
+                  {userStats?.best_case_fund_available.toFixed(2).split(".")[1]}
+                </span>
+              </span>
+            </div>
 
             {/* Cumulative Winnings */}
-            {cumulativeWinnings !== undefined && (
-              <div className="flex justify-between items-end">
-                <span>Cumulative winnings</span>
-                <span className="font-bold">
-                  <span className="text-[22px]">
-                    ${Math.floor(cumulativeWinnings).toLocaleString()}
-                  </span>
-                  <span className="text-[14px]">
-                    .{(cumulativeWinnings % 1).toFixed(2).split(".")[1]}
-                  </span>
+            <div className="flex justify-between items-end">
+              <span>Cumulative winnings</span>
+              <span className="font-bold">
+                <span className="text-[22px]">
+                  $
+                  {
+                    userStats?.best_case_cumulative_profit
+                      .toLocaleString()
+                      .split(".")[0]
+                  }
                 </span>
-              </div>
-            )}
+                <span className="text-[14px]">
+                  .
+                  {
+                    userStats?.best_case_cumulative_profit
+                      .toFixed(2)
+                      .split(".")[1]
+                  }
+                </span>
+              </span>
+            </div>
           </div>
 
-          {/* Bottom Dashed Border */}
-          <div className="border-t-2 border-dashed border-gray-400 w-full my-3"></div>
+          <div
+            className="border-t-2 border-dashed border-gray-400 w-full my-3"
+            style={{
+              maskImage:
+                "linear-gradient(to right, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.4))",
+              WebkitMaskImage:
+                "linear-gradient(to right, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.4))",
+            }}
+          ></div>
         </div>
-      )}
+      ) : null}
     </>
   );
 };
