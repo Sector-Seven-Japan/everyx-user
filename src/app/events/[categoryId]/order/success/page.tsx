75f299643fae2a66b33cce8589fdacbe7e5943b1
Sanjeev vishwakarma <143729099+Sanjiv99kv@users.noreply.github.com>
2025-02-28 14:30:45 +0530

    Update page.tsx

--- src/app/events/[categoryId]/order/success/page.tsx
+++ src/app/events/[categoryId]/order/success/page.tsx
@@ -212,7 +212,7 @@ export default function OrderSuccess() {
               <div className="mt-20 md:mt-[1.5vw]">
                 <div className="flex gap-3">
                   <button className="border border-[#00FFB8] px-4 py-2 text-xs text-[#2DC198] rounded-md md:rounded-sm md:py-[2px] md:text-[0.6vw]">
-                    {eventData?.category?.name?.split(" ")[0]}
+                    {eventData?.category?.name?.split(" ")[0] || "Global"}
                   </button>
                   <p className="text-[#2DC198] flex gap-1 items-center font-light">
                     <div className="md:w-3">
