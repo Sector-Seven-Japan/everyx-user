aa412e49ce3994b3fa729bb5b2e3645f66d59b1c
Deepak Yadav <123127859+dny5112001@users.noreply.github.com>
2025-03-01 08:34:29 +0530

    fixed the improvements

--- src/app/deposit-withdrawal/portfolio/page.tsx
+++ src/app/deposit-withdrawal/portfolio/page.tsx
@@ -11,6 +11,7 @@ import { useRouter } from "next/navigation";
 import { LuClock7 } from "react-icons/lu";
 import CurrentCashBalanceCardWebview from "@/components/CurrentCashBalanceWebview";
 import CashWithdrawalCategoriesMobile from "@/components/CashWithdrawalCategoriesMobile";
+import HeadingSlider from "@/components/HeadingSlider";
 
 interface BetEntry {
   id: string;
@@ -357,95 +358,148 @@ const Portfolio: React.FC = () => {
           </div>
         </div>
       ) : (
-        <div className="bg-[#0E0E0E] md:px-[12%] 2xl:px-[19%]">
-          <div className="flex md:flex-row md:mt-32 justify-between gap-5">
-            <div className="bg-[#0E0E0E] text-white px-5  pb-5 md:w-[60%] xl:w-[75%] w-full">
-              <div className="flex justify-between items-start">
-                <div>Results : </div>
-                <div className="flex items-start gap-4">
-                  <p className="text-[14px] text-left font-semibold">
-                    Results :
-                  </p>
-                  <div className="flex flex-col items-end  gap-2">
-                    <div className="flex w-36 bg-[rgba(255,255,255,0.1)] rounded-md justify-between items-center px-2 py-1">
-                      <div className="text-[14px]">{filter}</div>
-                      <div
-                        className="flex justify-center items-center flex-col gap-1 cursor-pointer"
-                        onClick={() => {
-                          setOpen((con) => !con);
-                        }}
-                      >
-                        <div className="w-[20px] h-[1.5px] bg-white"></div>
-                        <div className="w-[18px] h-[1.5px] bg-white"></div>
-                        <div className="w-[12px] h-[1.5px] bg-white"></div>
-                      </div>
-                    </div>
-                    <div
-                      className={`w-24 flex flex-col  right-10  ${
-                        open ? "block" : "hidden"
-                      }`}
-                    >
-                      <div
-                        onClick={() => {
-                          setFilter("All");
-                          setOpen(false);
-                        }}
-                        className="bg-transparent hover:bg-white/10 transition duration-300  pl-5 text-[14px] cursor-pointer"
-                      >
-                        All
+        <div className="bg-[#0E0E0E] w-full min-h-screen text-white relative">
+          <div className="md:px-[12%] 2xl:px-[19%]">
+            <HeadingSlider filter={filter} setFilter={setFilter} />
+            <div className="flex md:flex-row md:mt-10 justify-between gap-5">
+              <div className="bg-[#0E0E0E] text-white px-5  pb-5 md:w-[60%] xl:w-[75%] w-full">
+                <div className="flex justify-between items-start">
+                  <div>Results : </div>
+                  <div className="flex items-start gap-4">
+                    <p className="text-[14px] text-left font-semibold">
+                      Results :
+                    </p>
+                    <div className="flex flex-col items-end  gap-2">
+                      <div className="flex w-36 bg-[rgba(255,255,255,0.1)] rounded-md justify-between items-center px-2 py-1">
+                        <div className="text-[14px]">{filter}</div>
+                        <div
+                          className="flex justify-center items-center flex-col gap-1 cursor-pointer"
+                          onClick={() => {
+                            setOpen((con) => !con);
+                          }}
+                        >
+                          <div className="w-[20px] h-[1.5px] bg-white"></div>
+                          <div className="w-[18px] h-[1.5px] bg-white"></div>
+                          <div className="w-[12px] h-[1.5px] bg-white"></div>
+                        </div>
                       </div>
                       <div
-                        onClick={() => {
-                          setFilter("Active");
-                          setOpen(false);
-                        }}
-                        className="bg-transparent hover:bg-white/10 transition duration-300 pl-5 text-[14px] cursor-pointer"
+                        className={`w-24 flex flex-col  right-10  ${
+                          open ? "block" : "hidden"
+                        }`}
                       >
-                        Active
-                      </div>
-                      <div
-                        onClick={() => {
-                          setFilter("Success");
-                          setOpen(false);
-                        }}
-                        className="bg-transparent hover:bg-white/10 transition duration-300 pl-5 text-[14px] cursor-pointer"
-                      >
-                        Success
-                      </div>
-                      <div
-                        onClick={() => {
-                          setFilter("Failed");
-                          setOpen(false);
-                        }}
-                        className="bg-transparent hover:bg-white/10 transition duration-300 pl-5 text-[14px] cursor-pointer"
-                      >
-                        Failed
+                        <div
+                          onClick={() => {
+                            setFilter("All");
+                            setOpen(false);
+                          }}
+                          className="bg-transparent hover:bg-white/10 transition duration-300  pl-5 text-[14px] cursor-pointer"
+                        >
+                          All
+                        </div>
+                        <div
+                          onClick={() => {
+                            setFilter("Active");
+                            setOpen(false);
+                          }}
+                          className="bg-transparent hover:bg-white/10 transition duration-300 pl-5 text-[14px] cursor-pointer"
+                        >
+                          Active
+                        </div>
+                        <div
+                          onClick={() => {
+                            setFilter("Success");
+                            setOpen(false);
+                          }}
+                          className="bg-transparent hover:bg-white/10 transition duration-300 pl-5 text-[14px] cursor-pointer"
+                        >
+                          Success
+                        </div>
+                        <div
+                          onClick={() => {
+                            setFilter("Failed");
+                            setOpen(false);
+                          }}
+                          className="bg-transparent hover:bg-white/10 transition duration-300 pl-5 text-[14px] cursor-pointer"
+                        >
+                          Failed
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="max-w-2xl mx-auto space-y-8 mt-24">
-                {filteredBets.length > 0 ? (
-                  filteredBets.map((bet) => {
-                    const isExpired = new Date(bet.ends_at) < new Date();
+                <div className="max-w-2xl mx-auto space-y-8 mt-24">
+                  {filteredBets.length > 0 ? (
+                    filteredBets.map((bet) => {
+                      const isExpired = new Date(bet.ends_at) < new Date();
 
-                    // If event is expired, display each position separately
-                    if (isExpired) {
-                      return bet.position.map((pos) => (
-                        <div key={pos.id} className="space-y-3 mb-5">
+                      // If event is expired, display each position separately
+                      if (isExpired) {
+                        return bet.position.map((pos) => (
+                          <div key={pos.id} className="space-y-3 mb-5">
+                            {/* Status Indicator */}
+                            <div className="inline-block">
+                              {pos.indicative_return > 100 ? (
+                                <span className="px-3 py-1 rounded text-sm text-[#2dc198] border border-[#2dc198]">
+                                  Success
+                                </span>
+                              ) : (
+                                <span className="px-3 py-1 rounded text-sm text-orange-500 border border-orange-500">
+                                  Failed
+                                </span>
+                              )}
+                            </div>
+
+                            {/* Event Question */}
+                            <h2 className="text-white text-lg font-medium">
+                              {bet.question}
+                            </h2>
+
+                            {/* Ended Time */}
+                            <div className="flex justify-between items-center">
+                              <div className="text-gray-500 text-sm">
+                                {format(
+                                  new Date(bet.ends_at),
+                                  "MMM d yyyy HH:mm"
+                                )}
+                              </div>
+                              <div
+                                className={
+                                  pos.indicative_return > 100
+                                    ? "text-[#2dc198] text-sm"
+                                    : "text-orange-500 text-sm"
+                                }
+                              >
+                                $
+                                {Number(
+                                  pos.indicative_payout - pos.wager
+                                ).toFixed(2)}
+                              </div>
+                            </div>
+
+                            {/* Dotted Line Separator */}
+                            <div
+                              className="border-t-2 border-dashed border-gray-400 w-full mt-4"
+                              style={{
+                                maskImage:
+                                  "linear-gradient(to right, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.4))",
+                                WebkitMaskImage:
+                                  "linear-gradient(to left, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.4))",
+                              }}
+                            ></div>
+                          </div>
+                        ));
+                      }
+
+                      // If event is still active, display all positions under the same event
+                      return (
+                        <div key={bet.id} className="space-y-3 mb-5">
                           {/* Status Indicator */}
                           <div className="inline-block">
-                            {pos.indicative_return > 100 ? (
-                              <span className="px-3 py-1 rounded text-sm text-[#2dc198] border border-[#2dc198]">
-                                Success
-                              </span>
-                            ) : (
-                              <span className="px-3 py-1 rounded text-sm text-orange-500 border border-orange-500">
-                                Failed
-                              </span>
-                            )}
+                            <span className="px-3 py-1 rounded text-sm border-[#5dff00] border text-[#5dff00]">
+                              Active
+                            </span>
                           </div>
 
                           {/* Event Question */}
@@ -453,25 +507,44 @@ const Portfolio: React.FC = () => {
                             {bet.question}
                           </h2>
 
-                          {/* Ended Time */}
-                          <div className="flex justify-between items-center">
-                            <div className="text-gray-500 text-sm">
-                              {format(
-                                new Date(bet.ends_at),
-                                "MMM d yyyy HH:mm"
-                              )}
-                            </div>
-                            <div
-                              className={
-                                pos.indicative_return > 100
-                                  ? "text-[#2dc198] text-sm"
-                                  : "text-orange-500 text-sm"
-                              }
-                            >
-                              $
-                              {Number(
-                                pos.indicative_payout - pos.wager
-                              ).toFixed(2)}
+                          {/* Grouped Positions for Active Events */}
+                          <div className="mt-4">
+                            <h3 className="text-gray-300 text-sm font-semibold">
+                              Bets Placed:
+                            </h3>
+                            <ul className="mt-2 space-y-2">
+                              {bet.position.map((pos) => (
+                                <li
+                                  onClick={() => {
+                                    setIsLoading(true);
+                                    router.push(`/wager/${pos?.id}`);
+                                  }}
+                                  key={pos.id}
+                                  className="bg-white/10 p-3 rounded-md flex justify-between items-center"
+                                >
+                                  <span className="text-white text-sm">
+                                    {pos.event_outcome_id} :{" "}
+                                    {pos.event_outcome_name}
+                                  </span>
+                                  <div className="flex justify-between items-center gap-2">
+                                    <span className="text-[#5dff00] font-semibold">
+                                      $ {pos.pledge.toFixed(2)}{" "}
+                                      <span className="text-[9px] text-[#5dff00]">
+                                        (USDT)
+                                      </span>
+                                    </span>
+
+                                    <span className="text-[#b27c25] font-semibold">
+                                      x{pos.leverage.toFixed(1)}
+                                    </span>
+                                  </div>
+                                </li>
+                              ))}
+                            </ul>
+                            {/* Time Remaining */}
+                            <div className="text-[12px] text-[#2dc198] flex items-center gap-1 mt-5">
+                              <LuClock7 color="#2dc198" />{" "}
+                              {timeRemaining(bet.ends_at)}
                             </div>
                           </div>
 
@@ -486,86 +559,19 @@ const Portfolio: React.FC = () => {
                             }}
                           ></div>
                         </div>
-                      ));
-                    }
-
-                    // If event is still active, display all positions under the same event
-                    return (
-                      <div key={bet.id} className="space-y-3 mb-5">
-                        {/* Status Indicator */}
-                        <div className="inline-block">
-                          <span className="px-3 py-1 rounded text-sm border-[#5dff00] border text-[#5dff00]">
-                            Active
-                          </span>
-                        </div>
-
-                        {/* Event Question */}
-                        <h2 className="text-white text-lg font-medium">
-                          {bet.question}
-                        </h2>
-
-                        {/* Grouped Positions for Active Events */}
-                        <div className="mt-4">
-                          <h3 className="text-gray-300 text-sm font-semibold">
-                            Bets Placed:
-                          </h3>
-                          <ul className="mt-2 space-y-2">
-                            {bet.position.map((pos) => (
-                              <li
-                                onClick={() => {
-                                  setIsLoading(true);
-                                  router.push(`/wager/${pos?.id}`);
-                                }}
-                                key={pos.id}
-                                className="bg-white/10 p-3 rounded-md flex justify-between items-center"
-                              >
-                                <span className="text-white text-sm">
-                                  {pos.event_outcome_id} :{" "}
-                                  {pos.event_outcome_name}
-                                </span>
-                                <div className="flex justify-between items-center gap-2">
-                                  <span className="text-[#5dff00] font-semibold">
-                                    $ {pos.pledge.toFixed(2)}{" "}
-                                    <span className="text-[9px] text-[#5dff00]">
-                                      (USDT)
-                                    </span>
-                                  </span>
-
-                                  <span className="text-[#b27c25] font-semibold">
-                                    x{pos.leverage.toFixed(1)}
-                                  </span>
-                                </div>
-                              </li>
-                            ))}
-                          </ul>
-                          {/* Time Remaining */}
-                          <div className="text-[12px] text-[#2dc198] flex items-center gap-1 mt-5">
-                            <LuClock7 color="#2dc198" />{" "}
-                            {timeRemaining(bet.ends_at)}
-                          </div>
-                        </div>
-
-                        {/* Dotted Line Separator */}
-                        <div
-                          className="border-t-2 border-dashed border-gray-400 w-full mt-4"
-                          style={{
-                            maskImage:
-                              "linear-gradient(to right, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.4))",
-                            WebkitMaskImage:
-                              "linear-gradient(to left, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.4))",
-                          }}
-                        ></div>
-                      </div>
-                    );
-                  })
-                ) : (
-                  <p className="text-left text-gray-400">No bets available.</p>
-                )}
+                      );
+                    })
+                  ) : (
+                    <p className="text-left text-gray-400">
+                      No bets available.
+                    </p>
+                  )}
+                </div>
               </div>
-            </div>
-            <div className="md:w-[40%] xl:w-[25%]">
-              <div className="sticky top-20">
-                <CurrentCashBalanceCardWebview />
+              <div className="md:w-[40%] xl:w-[25%]">
+                <div className="sticky top-20">
+                  <CurrentCashBalanceCardWebview />
+                </div>
               </div>
             </div>
           </div>
